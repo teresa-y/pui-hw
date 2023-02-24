ae5cd5fec440c2dccf46f70245eb00779d9e57cd
teresa-y <tyang218@gmail.com>
2023-02-23 21:20:12 -0500

    did hw 4

--- solution-hw4/js/app.js
+++ solution-hw4/js/app.js
@@ -1,4 +1,3 @@
-let basePrice = 2.49;
 let glazingPrice = 0;
 let sizePrice = 1;
 
@@ -59,17 +58,50 @@ function sizeChange(element){
     displayPrice()
 }
 
-//url stuff
+//url params
 
 let cart = [];
 const queryString = window.location.search;
 const params = new URLSearchParams(queryString);
 const rollType = params.get('roll');
 
-basePrice = roll.basPrice;
+let basePrice = rolls[rollType].basePrice;
 
 console.log(document.querySelector('.item-img'));
 
 document.querySelector('.item-img img').setAttribute('src', `products/${rolls[rollType].imageFile}`);
 document.querySelector('.product-name').innerText = `${rollType} Cinnamon Roll`;
+document.title = `${rollType} Cinnamon Roll`;
+displayPrice()
+
+//add to cart
+class Roll {
+
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+    }
+}
+
+let selectGlazing = document.querySelector('#glazingOption');
+let selectSize = document.querySelector('#sizeOption');
+
+function addToCart() {
+    let rollGlazing = allGlaze[selectGlazing.selectedIndex];
+    let packSize = allSize[selectSize.selectedIndex];
+    cart.push(new Roll(rollType, rollGlazing, packSize, basePrice));
+    console.log(cart);
+}
+
+selectGlazing.addEventListener('change', onSelectValueChange)
+selectSize.addEventListener('change', onSelectValueChange)
+
+document.querySelector('#form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('hi');
+    addToCart();
+  })
+
 
